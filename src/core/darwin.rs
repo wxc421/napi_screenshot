170905ea1144edf564612f8494f8d7ee3146852d
wxc <962447907@qq.com>
2023-07-20 14:27:49 +0800

    fix: bug

--- src/core/darwin.rs
+++ src/core/darwin.rs
@@ -4,5 +4,5 @@ use crate::core::DisplayInfo;
 use crate::core::image::Image;
 
 pub fn capture_screen(display_info: &DisplayInfo) -> Result<Image> {
-    Err(anyhow!("not implemented"));
+    Err(anyhow!("not implemented"))
 }
