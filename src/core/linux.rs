9acc123903373541de0069023d43e38d9cba2d45
wxc <962447907@qq.com>
2023-07-20 14:22:03 +0800

    fix: bug

--- src/core/linux.rs
+++ src/core/linux.rs
@@ -1,5 +1,8 @@
 use anyhow::{anyhow, Result};
 
+use crate::core::DisplayInfo;
+use crate::core::image::Image;
+
 pub fn capture_screen(display_info: &DisplayInfo) -> Result<Image> {
     Err(anyhow!("not implemented"));
 }
